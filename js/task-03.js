13466d11d4c7881da3e21a066950392547af9728
keastar <keastar1986@gmail.com>
2023-01-21 22:24:35 +0200

    done 4 task
    done 4 task

--- js/task-03.js
+++ js/task-03.js
@@ -13,12 +13,20 @@ const images = [
   },
 ];
 
-//Создание коллекции разметки элементов: 
+const GalleryEl = document.querySelector('.gallery');
+
+const elements = images
+  .map((image) => `<li class = "mySlides fade" ><img class = "gallery_img" src = "${image.url}" alt = "${image.alt}" > </li>`)
+  .join("");
+
+console.log(elements);
+GalleryEl.insertAdjacentHTML("afterbegin", elements);
+
+
+// Создание коллекции разметки элементов: 
 // Получаем массив элементов, где для каждой картинки
 // создаем li и img теги, которые по итогу вставляем
 // в дерево DOM в ul с классом "gallery".
-
-const GalleryEl = document.querySelector('.gallery');
 
 // const elements = images.map(image => {
 //   const LiForImgEl = document.createElement('li');
@@ -37,73 +45,31 @@ const GalleryEl = document.querySelector('.gallery');
 // console.log(elements);
 // GalleryEl.append(...elements);
 
+
+
 // Напишем функцию, которая будет получать массив из 3х картинок и
 // возвращать массив разметки для этих трех картинок в виде li и img тегов
 // и вставлять этот массив в HTML.
 
-const makeImagesMass = (images) => {
-  return images.map(image => {
-    const LiForImgEl = document.createElement('li');
-    LiForImgEl.classList.add('mySlides');
-    LiForImgEl.classList.add('fade');
+// const makeImagesMass = (images) => {
+//   return images.map(image => {
+//     const LiForImgEl = document.createElement('li');
+//     LiForImgEl.classList.add('mySlides');
+//     LiForImgEl.classList.add('fade');
 
-    const ImageEl = document.createElement('img');
-    ImageEl.classList.add('gallery_img');
-    ImageEl.src = image.url;
-    ImageEl.alt = image.alt;
+//     const ImageEl = document.createElement('img');
+//     ImageEl.classList.add('gallery_img');
+//     ImageEl.src = image.url;
+//     ImageEl.alt = image.alt;
 
-    LiForImgEl.appendChild(ImageEl);
-    return LiForImgEl;
-  });
-};
+//     LiForImgEl.appendChild(ImageEl);
+//     return LiForImgEl;
+//   });
+// };
 
-const elements = makeImagesMass(images);
-GalleryEl.append(...elements);
-console.log(elements);
+// const elements = makeImagesMass(images);
+// GalleryEl.append(...elements);
+// console.log(elements);
 
 
-
-// const GalleryEl = document.querySelector('.gallery');
-// GalleryEl.appendChild(LiForImgEl);
-// GalleryEl.insertAdjacentHTML('afterbegin', ImageEl);
-
-// const ButPrevEl = document.createElement('a');
-// ButPrevEl.classList.add('prev');
-// ButPrevEl.textContent('&#10094');
-// ButPrevEl.onclick(plusSlides(-1));
-
-// const ButNextEl = document.createElement('a');
-// ButNextEl.classList.add('next');
-// ButPrevEl.textContent('#10095');
-// ButNextEl.onclick(plusSlides(1));
-
-// document.body.appendChild(ButPrevEl);
-// document.body.appendChild(ButNextEl);
-
-// // let slideIndex = 1;
-// // showSlides(slideIndex);
-
-// // function plusSlides(n) {
-// //   showSlides(slideIndex += n);
-// // }
-
-// // function currentSlide(n) {
-// //   showSlides(slideIndex = n);
-// // }
-
-// // function showSlides(n) {
-// //   let i;
-// //   let slides = document.getElementsByClassName("mySlides");
-// //   let dots = document.getElementsByClassName("dot");
-// //   if (n > slides.length) {slideIndex = 1}    
-// //   if (n < 1) {slideIndex = slides.length}
-// //   for (i = 0; i < slides.length; i++) {
-// //       slides[i].style.display = "none";  
-// //   }
-// //   for (i = 0; i < dots.length; i++) {
-// //       dots[i].className = dots[i].className.replace("active", "");
-// //   }
-// //   slides[slideIndex-1].style.display = "block";  
-// //   dots[slideIndex-1].className += "active";
-// // }
 
---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+const counterValueEl = document.querySelector('#value');
+const addCountBtn = document.querySelector('[data-action="increment"]');
+const removeCountBtn = document.querySelector('[data-action="decrement"]');
+
+let counterValue = 0;
+function increment() {
+    counterValue += 1;
+    counterValueEl.innerHTML = counterValue;
+}
+
+addCountBtn.addEventListener("click", increment);
+
+function decrement() {
+	counterValue -= 1;
+    counterValueEl.innerHTML = counterValue;
+}
+
+removeCountBtn.addEventListener("click", decrement);
