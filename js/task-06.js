1683e4169814ece48b576c7dff5a1c12698877dd
keastar <keastar1986@gmail.com>
2023-01-23 17:59:52 +0200

    done 6 task
    done 6 task

---
+++ js/task-06.js
@@ -0,0 +1,24 @@
+const inputEl = document.querySelector('#validation-input');
+const inputLength = Number(inputEl.getAttribute('data-length'));
+
+
+inputEl.addEventListener('change', onInputChange);
+inputEl.addEventListener('blur', onInputBlur);
+
+function onInputChange(event) {
+    console.log(event.currentTarget.value);
+};
+
+function onInputBlur(event) {
+    console.log(event.currentTarget.value.length);
+    console.log(inputLength);
+
+    if (event.currentTarget.value.length !== inputLength && !inputEl.classList.contains('invalid')) {
+        inputEl.classList.add('invalid');
+    }
+    else if (event.currentTarget.value.length === inputLength) {
+        inputEl.classList.remove('invalid');
+        inputEl.classList.add('valid');
+  }
+    
+}
