9197ee677bf27c522d63976bcd859b374a458b75
keastar <keastar1986@gmail.com>
2023-01-24 12:22:40 +0200

    done 7 task
    done 7 task

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const controlSizeEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+controlSizeEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    textEl.style.fontSize = event.currentTarget.value + "px";
+}
