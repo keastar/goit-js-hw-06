c850d821213d8b503a0bbb4e5724b146ea9ed213
keastar <keastar1986@gmail.com>
2023-01-30 21:29:02 +0200

    done 9 task
    done 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const buttonEl = document.querySelector('.change-color');
+const codeColorEl = document.querySelector('.color');
+const bgColorEl = document.querySelector('body');
+
+buttonEl.addEventListener('click', (event) => {
+  bgColorEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  
+  codeColorEl.textContent = bgColorEl.style.backgroundColor;
+    
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,10 @@
+
+
+
+
+function createBoxes(amount)
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
