0b91794692dccae0cd9ad0c46d991e44ac99df65
keastar <keastar1986@gmail.com>
2023-02-03 14:37:46 +0200

    Done task10
    Done task10

--- js/task-09.js
+++ js/task-09.js
@@ -2,17 +2,16 @@ const buttonEl = document.querySelector('.change-color');
 const codeColorEl = document.querySelector('.color');
 const bgColorEl = document.querySelector('body');
 
-buttonEl.addEventListener('click', (event) => {
-  bgColorEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-  
-  codeColorEl.textContent = bgColorEl.style.backgroundColor;
-    
-});
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+buttonEl.addEventListener('click', (event) => {
+  bgColorEl.style.backgroundColor = getRandomHexColor();
+  codeColorEl.textContent = bgColorEl.style.backgroundColor;
+    
+});
+
+
--- js/task-10.js
+++ js/task-10.js
@@ -1,10 +1,31 @@
+const refs = {
+  contentEl: document.querySelector('#boxes'),
+  inputEl: document.querySelector('input'),
+};
+const buttonCr = document.querySelector('#createBtn');
+const buttonDs = document.querySelector('#destroyBtn');
 
-
-
-
-function createBoxes(amount)
-
+buttonCr.addEventListener('click', onClickBtnCr);
+buttonDs.addEventListener('click', onClickBtnDs);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickBtnCr() {
+  const n = refs.inputEl.value;
+  let wh = 30;
+  for (let i = 1; i <= n; i++) {
+    const divEl = document.createElement('div');
+    divEl.style.background = getRandomHexColor();
+    divEl.style.width = wh+'px';
+    divEl.style.height = wh+'px';
+    refs.contentEl.appendChild(divEl);
+    wh += 10;
+  }
+  console.log('contentEl');
+}
+
+function onClickBtnDs() {
+  refs.contentEl.innerHTML = '';
 }
